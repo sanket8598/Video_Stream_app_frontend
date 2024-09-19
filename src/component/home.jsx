215affc6e7aefdebb0ee469d52bb65fcefba2bb8
Sanket Wakankar <Sanket Wakankar@ho.rnt.ai>
2024-09-19 19:41:48 +0530

    added the initial commit

---
+++ src/component/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home=()=>{
+    return (
+        <>
+        <div className="flex justify-center">
+          <h1 className="text-5xl font-extrabold text-gray-700 dark:text-gray-100">Welcome to the video streaming application</h1>
+        </div>
+        </>
+    )
+}
+
+export default Home;
